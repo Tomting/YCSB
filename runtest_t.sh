306e698ac67e182702baf739b8817e4cba2e0199
n-daniel <daniel.nardin@gmail.com>
2013-04-10 11:48:21 +0300

    Create runtest_t.sh

---
+++ runtest_t.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# if not set THREADS use default
+if [ -z "$THREADS" ]; then
+  THREADS=1
+fi
+
+# if not set GRANULARITY use default (1sec)
+if [ -z "$GRANULARITY" ]; then
+  GRANULARITY=1000
+fi
+
+
+# clean up 
+CLEANUP=$1.$THREADS.* 
+for f0 in $CLEANUP
+do
+  echo "delete $f0"
+	rm $f0
+done
+
+# execute test
+FILES=workloads.1/*
+for f in $FILES
+do
+	now=$(date +"%Y%m%d_%H_%M_%S")
+	bin/ycsb load $1 -P $f $2 $3 $4 $5 $6 $7 $8 $9 -p table=$(basename $f) > load_$1.$THREADS.$(basename $f).$now
+	bin/ycsb run $1 -P $f $2 $3 $4 $5 $6 $7 $8 $9 -threads $THREADS -p table=$(basename $f) -p measurementtype=timeseries -p timeseries.granularity=$GRANULARITY > $1.$THREADS.$(basename $f).$now
+done
